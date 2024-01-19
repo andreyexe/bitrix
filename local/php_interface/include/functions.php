8470cb11d50c03267c0ee9d75e63c17ee988f308
Android0304 <mknb-191_487867@volsu.ru>
2024-01-19 13:44:38 +0300

    course 1 lesson 4-2

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -1,8 +1,26 @@
 <?php
+// для себя
 function debug($var, $die = false, $all = false)
 {
     global $USER;
     if (($USER->GetID() == 1) || ($all == true)) {
+        ?>
+        <font style="text-align: left; font-size: 10px">
+            <pre>
+                <?php var_dump($var); ?>
+            </pre>
+        </font><br>
+        <?php
+    }
+    if ($die) {
+        die;
+    }
+}
+// переделанная по заданию
+function dump($var, $die = false, $all = false)
+{
+    global $USER;
+    if ( ($USER->IsAdmin()) || ($all == true)) {
         ?>
         <font style="text-align: left; font-size: 10px">
             <pre>
